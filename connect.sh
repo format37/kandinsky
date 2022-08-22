df429e55703f58f185a3f032f7a1921dc1a9756c
alex <format37@gmail.com>
2022-08-22 19:59:44 +0300

    modified:   connect.sh

--- connect.sh
+++ connect.sh
@@ -1,4 +1,4 @@
 # get id of container with name kandinsky_generator_1 and store to variable id
-id=$(docker ps -a | grep kandinsky_generator_1 | awk '{print $1}')
+id=$(sudo docker ps -a | grep kandinsky_generator_1 | awk '{print $1}')
 # connect bash to container with id $id
-docker exec -it $id /bin/bash
+sudo docker exec -it $id /bin/bash
