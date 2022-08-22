cad2a70c8d367503e84e0259ea587b4aeb9aa466
alex <format37@gmail.com>
2022-08-22 19:37:02 +0300

    modified:   compose.sh

--- compose.sh
+++ compose.sh
@@ -3,7 +3,7 @@ mkdir -p data/generated
 mkdir -p data/models
 # if path data/models/rudalle is not exist, then download the model
 if [ ! -d "data/models/Kandinsky" ]; then
-    wget 'http://95.165.139.53:42080/kandinsky_cache.tar.gz' -O data/models/kandinsky_cache.tar.gz
+    wget 'http://95.165.139.53:42080/models/kandinsky_cache.tar.gz' -O data/models/kandinsky_cache.tar.gz
     # unpack the model
     tar -xvf data/models/kandinsky_cache.tar.gz -C data/models/
     # remove the model archive
